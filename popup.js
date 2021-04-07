70c1cacdcbb508fb9c7fbf09d2746e47f22db3b0
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2021-04-07 12:56:23 -0300

    small changes

--- popup.js
+++ popup.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    //Do something
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    await displayBills();
+
 });
+
+const displayBills = async () => {
+    const savedBills = await BillService.getBills();
+    
+    const items = document.getElementById('items-list');
+    const emptyElement = document.getElementById('empty-list');
+    const showMoreButton = document.getElementById('show-more');
+
+    if (savedBills === []) {
+        showMoreButton.setAttribute('hidden');
+    } else {
+        showMoreButton.removeAttribute('hidden');
+    }
+}
+
